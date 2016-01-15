5f7e02457ca54e29fe457d3b6cdf3891a50325d0
Graeme Harrison <gharrison365@gmail.com>
2016-01-15 14:51:46 -0500

    First commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Graeme Harrison on 2016-01-15.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
